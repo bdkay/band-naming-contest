97eda619dde6d05e04cfc66484e79548ee264f96
Brendon Kuleck <brendonkuleck@gmail.com>
2017-02-28 15:34:18 -0800

    Added some views, also updated package.json

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-import config, { nodeEnv } from './config';
+import config from './config';
+import apiRouter from './api';
 
-console.log(config, nodeEnv);
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index', {
+    content: '...'
+  });
+});
+
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port', config.port);
+});
